1f91951910e511f6c3f974b2549dafe8fc002764
Gytis Daujotas <gytdau@gmail.com>
2017-07-14 17:31:11 +0100

    :memo: Added better documentation on setting up

--- holiday_bot/slackbot_settings.example.py
+++ holiday_bot/slackbot_settings.example.py
@@ -16,7 +16,9 @@ PLUGINS = [
 API_AI_KEY = "api.ai client access token"
 API_TOKEN = "slack integration api key"
 
-GOOGLE_CALENDAR_ID="something@group.calendar.google" # Look up how to find the Google Calendar ID of a specific calendar.
+GOOGLE_CALENDAR_ID="something@group.calendar.google"
+# Find this by going into Google Calendar for Web, clicking the dropdown beside a specific calendar,
+# going into Settings,  and you'll find the ID on that page in that email format.
 
 # Small inconsequential note: the bot needs to first be messaged after it starts
 # before it can send the daily list. Anyone can message it, and they don't
@@ -30,4 +32,7 @@ SEND_TO = "pto" # send the week's calendar at 9am to the channel #pto
 # located in client_secret.json in this folder
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
+# client_secret.json is a JSON file you download from Google.
+# Follow Step 1. of this guide to do so:
+# https://developers.google.com/google-apps/calendar/quickstart/python
 APPLICATION_NAME = 'PTO Thingy'
